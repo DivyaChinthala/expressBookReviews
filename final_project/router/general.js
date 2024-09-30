63ba3e893809bcb9382ac4b571dcf91ada5a8909
DivyaChinthala <divya519ch@gmail.com>
2024-09-30 07:29:18 -0400

    feat: task3 change

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,7 +42,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const filteredBooks = []
+  Object.keys(books).filter((key) => {
+    const book = books[key]
+    if(book.author == author) {
+        filteredBooks.push(book)
+    }
+  });
+  return res.status(200).json(filteredBooks);
 });
 
 // Get all books based on title
